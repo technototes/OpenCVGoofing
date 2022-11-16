c843200f00ca8fbace49dcc9ad55770d0e30f90b
Kevin Frei <kevinfrei@hotmail.com>
2022-11-15 21:20:23 -0800

    Added a sample image

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,9 +12,10 @@ public class Main {
         return imageCodecs.imread(path);
     }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // This dumps the 'home' directory of the program
+        System.out.println(System.getProperty("user.dir"));
         OpenCV.loadShared();
-        Mat im = loadImage(args[0]);
+        Mat im = loadImage("sample.jpg");
         Mat hsv = new Mat();
         // To make this fast, we could actually do this line-by-line,
         // instead of converting the whole image...
@@ -22,5 +23,6 @@ public class Main {
         // hsv is now the matrix for the image
         // Start looking at the *bottom* and search for some number of
         // consecutive yellow pixels to determine if that's a junction
+        // From the sample image, you'll notice that there's a reflection to be careful about...
     }
 }
