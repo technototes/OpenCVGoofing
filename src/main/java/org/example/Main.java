1b7e982418ca3e6af147252051298fb007a546ca
FTC16750 <technototes@outlook.com>
2022-11-18 17:41:24 -0800

    started with getting a align to junction code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,19 @@ public class Main {
         Imgcodecs imageCodecs = new Imgcodecs();
         return imageCodecs.imread(path);
     }
+
+    public static double YELLOW = 25;
+    public static double lowS = 60;
+    // The high saturation point for color identification
+    public static double highS = 255;
+    // The low value for color ID
+    public static double lowV = 50;
+    // The high value for color ID
+    public static double highV = 255;
+
     public static void main(String[] args) {
+
+
         // This dumps the 'home' directory of the program
         System.out.println(System.getProperty("user.dir"));
         OpenCV.loadLocally();
@@ -20,9 +32,24 @@ public class Main {
         // To make this fast, we could actually do this line-by-line,
         // instead of converting the whole image...
         Imgproc.cvtColor(im, hsv, Imgproc.COLOR_RGB2HSV);
+        int count = 0;
         // hsv is now the matrix for the image
         // Start looking at the *bottom* and search for some number of
         // consecutive yellow pixels to determine if that's a junction
         // From the sample image, you'll notice that there's a reflection to be careful about...
+
+        for (int j = hsv.height() - 1; j > 0; j--) {
+            for (int i = hsv.width() - 1; i > 0; i--) {
+                double[] color = hsv.get(j, i);
+                if (color[0] < YELLOW + 10 && color[0] > YELLOW - 10 && color[0] > lowS - 10 && color[0] < highS +10 &&
+                color[0] > lowV - 10 && color[0] < highV) {
+                    count++;
+                }
+
+                    // Draw a dot on the image at this point - input was put into img
+                    // The color choice makes things stripey, which makes it easier to identif
+                }
+            }
+        }
     }
 }
